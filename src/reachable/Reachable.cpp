a26d2e5c9c63567d160d5dc83d53c6cfa93d315d
Hussien Abdo <habdo@rhrk.uni-kl.de>
2021-01-27 22:10:06 +0100

    update

--- src/reachable/Reachable.cpp
+++ src/reachable/Reachable.cpp
@@ -4,3 +4,36 @@
 
 #include "Reachable.h"
 
+namespace ClassProject {
+
+
+    Reachable::Reachable(unsigned int stateSize){
+        Manager m;
+        stateSize = stateSize;
+    }
+
+    BDD_ID Reachable::xnor2(BDD_ID a, BDD_ID b) {
+        return ite(a, b, neg(b));
+    }
+
+    const std::vector<BDD_ID> &Reachable::getStates() const{
+
+    }
+
+    void Reachable::setDelta(const std::vector<BDD_ID> &transitionFunctions){
+
+    }
+
+    void Reachable::setInitState(const std::vector<bool>& stateVector){
+
+    }
+
+    BDD_ID Reachable::compute_reachable_states(){
+
+    }
+
+    bool Reachable::is_reachable(const std::vector<bool>& stateVector){
+
+    }
+
+}
--- src/reachable/Reachable.h
+++ src/reachable/Reachable.h
@@ -13,7 +13,24 @@ namespace ClassProject {
 
     class Reachable : public ReachableInterface {
 
+    public:
 
+        Reachable(unsigned int stateSize);
+
+        BDD_ID xnor2(BDD_ID a, BDD_ID b);
+
+        const std::vector<BDD_ID> &getStates() const;
+
+        void setDelta(const std::vector<BDD_ID> &transitionFunctions);
+
+        void setInitState(const std::vector<bool>& stateVector);
+
+        BDD_ID compute_reachable_states();
+
+        bool is_reachable(const std::vector<bool>& stateVector);
+
+    private:
+        unsigned int stateSize;
     };
 
 }
